16f4c1ac863ae0b5f6a2946d2a673a86cffc7a12
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2022-11-23 15:55:00 +0100

    Add documentation

--- instructions/Forward.hh
+++ instructions/Forward.hh
@@ -2,6 +2,9 @@
 
 #include "Instruction.hh"
 
+/**
+ * @brief An instruction that makes a turtle goes forward.
+ */
 class Forward : public Instruction {
     public:
         unsigned int numberOfArguments() const override;
