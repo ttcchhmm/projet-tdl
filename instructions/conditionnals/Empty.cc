d772fe599f56f49fb51daf2a2987347b7f389a19
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2022-12-20 19:30:41 +0100

    Add the empty conditional

---
+++ instructions/conditionnals/Empty.cc
@@ -0,0 +1,55 @@
+#include "Empty.hh"
+
+Empty::Empty(std::size_t turtle, CheckDirection const & dir): Instruction(turtle), _direction(dir) {}
+
+bool Empty::execute(JardinRendering* const & garden) {
+    float delta(0);
+
+    switch(_direction) {
+        case CheckDirection::FRONT: {
+            delta = 0;
+            break;
+        }
+
+        case CheckDirection::RIGHT: {
+            delta = 90;
+            break;
+        }
+
+        case CheckDirection::BACK: {
+            delta = 180;
+            break;
+        }
+
+        case CheckDirection::LEFT: {
+            delta = 270;
+            break;
+        }
+
+        default: { // Shouldn't happen
+            delta = 0;
+            break;
+        }
+    }
+
+    float orientation(garden->orientation(getTarget()) + delta);
+
+    // Stay in bounds
+    if(orientation > 360) {
+        orientation -= 360;
+    } else if(orientation < 0) {
+        orientation += 360;
+    }
+
+    if(orientation == 0) {
+        return !garden->estMur(garden->getTortues()[getTarget()]->getX(), garden->getTortues()[getTarget()]->getY() - 1);
+    } else if(orientation == 90) {
+        return !garden->estMur(garden->getTortues()[getTarget()]->getX() + 1, garden->getTortues()[getTarget()]->getY());
+    } else if(orientation == 180) {
+        return !garden->estMur(garden->getTortues()[getTarget()]->getX(), garden->getTortues()[getTarget()]->getY() + 1);
+    } else if(orientation == 270) {
+        return !garden->estMur(garden->getTortues()[getTarget()]->getX() - 1, garden->getTortues()[getTarget()]->getY());
+    }
+
+    return false;
+}
---
+++ instructions/conditionnals/Empty.hh
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../Instruction.hh"
+
+/**
+ * @brief Directions around a turtle.
+ */
+enum class CheckDirection {
+    /**
+     * @brief Front of the turtle.
+     */
+    FRONT,
+
+    /**
+     * @brief Back of the turtle.
+     */
+    BACK,
+
+    /**
+     * @brief Left of the turtle.
+     */
+    LEFT,
+
+    /**
+     * @brief Right of the turtle.
+     */
+    RIGHT
+};
+
+/**
+ * @brief An instruction that checks for a free space.
+ */
+class Empty: public Instruction {
+    private:
+        CheckDirection _direction;
+
+    public:
+        /**
+         * @brief Construct a new Empty instruction.
+         * 
+         * @param turtle The turtle to use.
+         * @param dir The direction to check.
+         */
+        Empty(std::size_t turtle, CheckDirection const & dir);
+        Empty(Empty const &) = default;
+        ~Empty() = default;
+
+        bool execute(JardinRendering* const & garden) override;
+};
