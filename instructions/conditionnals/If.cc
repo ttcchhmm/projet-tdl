ce5a22a8e758bac0232dbc95a8746d65857fdae3
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2022-12-20 19:32:07 +0100

    Add the if instruction

---
+++ instructions/conditionnals/If.cc
@@ -0,0 +1,15 @@
+#include "If.hh"
+
+If::If(std::shared_ptr<Instruction> cond, std::list<std::shared_ptr<Instruction>> instructions): Instruction(0), _cond(cond), _instructions(instructions) {}
+
+bool If::execute(JardinRendering* const & garden) {
+    if(_cond->execute(garden)) {
+        for(auto const & i : _instructions) {
+            if(!i->execute(garden)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
---
+++ instructions/conditionnals/If.hh
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../Instruction.hh"
+
+/**
+ * @brief The 'if' conditional.
+ */
+class If: public Instruction {
+    private:
+        /**
+         * @brief The condition.
+         */
+        std::shared_ptr<Instruction> _cond;
+
+        /**
+         * @brief The instructions to run.
+         */
+        std::list<std::shared_ptr<Instruction>> _instructions;
+
+    public:
+        /**
+         * @brief Construct a new If instruction.
+         * @param cond The condition.
+         * @param instructions The instructions if the condition is true.
+         */
+        If(std::shared_ptr<Instruction> cond, std::list<std::shared_ptr<Instruction>> instructions);
+        If(If const &) = default;
+        ~If() override = default;
+
+        bool execute(JardinRendering* const & garden) override;
+};
